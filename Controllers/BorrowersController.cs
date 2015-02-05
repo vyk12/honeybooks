88f43877cb57f90d52aaa4798de0c6fc6d8de9c9
Victor Thuillier <victor.thuillier@gmail.com>
2015-02-05 15:30:16 +0100

    prepare borrowers CRUD

--- Controllers/BorrowersController.cs
+++ Controllers/BorrowersController.cs
@@ -8,17 +8,38 @@ namespace HoneyBooks.Controllers
 {
     public class BorrowersController : Controller
     {
+        // Action to add a borrower
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        // Action to delete a borrower
+        public void Delete()
+        {
+
+        }
+
+        // Action to log in as a borrower
         public ActionResult Index()
         {
             return View();
         }
 
+        // Action to list borrowed books
         public ActionResult List()
         {
             return View();
         }
 
+        // Action to renew a loan
         public ActionResult RenewLoan()
+        {
+            return View();
+        }
+
+        // Action to update a borrower
+        public ActionResult Update()
         {
             return View();
         }
