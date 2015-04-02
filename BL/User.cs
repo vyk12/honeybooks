e846a06e6c7c84adc11b51806e44778e6f5ddf58
Victor Thuillier <victor.thuillier@gmail.com>
2015-04-02 02:55:01 +0200

    YOLO

--- BL/User.cs
+++ BL/User.cs
@@ -21,12 +21,15 @@ namespace BL
             set { this._id = value; }
         }
 
+        [Display(Name = "Username")]
         public string Username
         {
             get { return this._Username; }
             set { this._Username = value; }
         }
 
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
         public string Password
         {
             get { return this._Password; }
@@ -67,6 +70,126 @@ namespace BL
                 con.Close();
             }
             return user;
+        }
+
+        public static User getById(int id)
+        {
+            User user = null;
+            SqlConnection con = new SqlConnection(Settings.ConnectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM USERS WHERE id=" + id, con);
+
+            try
+            {
+                con.Open();
+                SqlDataReader dar = cmd.ExecuteReader();
+                if (dar.Read())
+                {
+                    user = new User();
+
+                    user._id = Convert.ToInt32(dar["id"]);
+                    user._Username = dar["Username"] as string;
+                    user._Password = dar["Password"] as string;
+                }
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return user;
+        }
+
+        public static List<User> getAll()
+        {
+            List<User> results = new List<User>();
+            SqlConnection con = new SqlConnection(Settings.ConnectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM USERS", con);
+            try
+            {
+                con.Open();
+                SqlDataReader dar = cmd.ExecuteReader();
+                while (dar.Read())
+                {
+                    User newUser = new User();
+                    newUser._id = Convert.ToInt32(dar["id"]);
+                    newUser._Username = dar["Username"] as string;
+                    newUser._Password = dar["Password"] as string;
+                    results.Add(newUser);
+                }
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return results;
+        }
+
+        public int save()
+        {
+            SqlConnection con = new SqlConnection(Settings.ConnectionString);
+            SqlCommand cmd;
+            int retVal = -1;
+            bool existing;
+            if (this._id == 0)
+            {
+                existing = false;
+                cmd = new SqlCommand("INSERT INTO USERS (Username, Password) VALUES ('" + this._Username + "','" + this._Password + "'); SELECT SCOPE_IDENTITY()", con);
+            }
+            else
+            {
+                existing = true;
+                cmd = new SqlCommand("UPDATE USERS SET Username='" + this._Username + "', Password='" + this._Password + "' WHERE id=" + this._id, con);
+            }
+            try
+            {
+                con.Open();
+                if (!existing)
+                {
+                    retVal = cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    retVal = -1;
+                    cmd.ExecuteScalar();
+                }
+
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return retVal;
+        }
+
+        public static void deleteById(int id)
+        {
+            SqlConnection con = new SqlConnection(Settings.ConnectionString);
+            SqlCommand cmd = new SqlCommand("DELETE FROM Users WHERE id = " + id, con);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteScalar();
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BL;
+
+namespace HoneyBooks.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Pages");
+            }
+
+            return View(BL.User.getAll());
+        }
+
+        public ActionResult Create()
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Pages");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Pages");
+            }
+
+            try
+            {
+                BL.User user = new BL.User();
+                user.Username = collection["Username"];
+                user.Password = Settings.SecureString(collection["Password"]);
+                user.save();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Pages");
+            }
+
+            User user = BL.User.getById(id);
+            user.Password = "";
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Pages");
+            }
+
+            try
+            {
+                BL.User user = BL.User.getById(id);
+                user.Username = collection["Username"];
+
+                if (collection["Password"] != "")
+                {
+                    user.Password = Settings.SecureString(collection["Password"]);
+                }
+
+                user.save();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Pages");
+            }
+
+            BL.User.deleteById(id);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
